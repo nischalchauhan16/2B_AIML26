43f1b4e3dc05b553f99b2ff6cab4cebc3aed01e9
PRARAK CHAUHAN <nischalchauhan16@gmail.com>
2026-09-08 23:25:14 +0530

    Add files via upload

---
+++ stringtempelate.js
@@ -0,0 +1,8 @@
+let firstName = "Niscchal"
+let lastName =  "Chauhan"
+
+console.log("My firstName is" + firstName)
+console.log("My lastName is" + firstName)
+
+console.log('My firstName is $  {firstName}');
+console.log(' "My firstName is :" $  {firstName} $ {lastName }');
