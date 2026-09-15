042d1f7833e85886d445a9ededdd329a75337f5b
PRARAK CHAUHAN <nischalchauhan16@gmail.com>
2026-09-15 19:30:15 +0530

    Add files via upload

---
+++ classdisplay.js
@@ -0,0 +1,13 @@
+class student {
+    static FName = "vikas";
+    static LName = "chaudhary";
+
+    static display() {
+        console.log(student.FName);
+        console.log(student.LName);
+    }
+}
+
+student.display();
+
+let s1 = new student();
